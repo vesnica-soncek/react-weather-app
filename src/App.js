0103b39f13590b315f5c1342f08292d29947549e
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-08 14:40:34 +0100

    Header - functionality OK, need some styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Menu from "./Components/Menu/Menu";
 import Logo from "./Components/Logo/Logo";
+import Header from "./Components/Header/Header";
 export function App() {
 
     const logo = new URL('./assets/logo.svg', import.meta.url);
 
     return (
         <>
-            <h1>Parcel React App</h1>
+            <Header>
             <Logo title="Weather by VicBox" url={logo} />
             <Menu items={[
                 { name: "Github", link: "/" },
                 { name: "About", link: "#about"}
             ]}/>
+            </Header>
+
             <p>Edit <code>src/App.tsx</code> to get started!</p>
         </>
     );
