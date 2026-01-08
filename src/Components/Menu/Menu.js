c156e4e49d3f867fb8a3fbf2bdda1cb5b784115e
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-08 13:45:55 +0100

    menu.js (statičen)

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu =() => {
+    return (
+        <nav className="nav">
+            <ul className="nav__lis">
+                <li className="nav__list-item">GitHub</li>
+                <li className="nav__list-item">About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu;
